078dc063c5895098ce2579099b86bc348b17e82e
bosslife19 <wokodavid001@gmail.com>
2026-01-19 14:04:05 +0100

    new

--- storage/framework/views/41805a6335cdaa0e91ff3062bbde5e50.php
+++
@@ -1,2 +0,0 @@
-
-<?php /**PATH C:\xampp\htdocs\schoolmanagement1\resources\views/themes/edulia/pagebuilder/app-banner/view.blade.php ENDPATH**/ ?>
--- storage/framework/views/ba601e0740021a0eecc689dd15c88a3f.php
+++
@@ -1,3 +0,0 @@
-   <!-- blog area start -->
-   
-<!-- blog area end --><?php /**PATH C:\xampp\htdocs\schoolmanagement1\resources\views/themes/edulia/pagebuilder/news-section/view.blade.php ENDPATH**/ ?>
